fa8f7a44c727c29b8c4d2f14867dc94a573643f1
GABRIEL APARECIDO DA SILVA <gabriel.asilva175@senacsp.edu.br>
2025-09-16 22:06:42 -0300

    finalização d atela de login

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -17,57 +15,89 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 30),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Digite seu login',
+      backgroundColor: Colors.grey[50],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/TitoGo.png', height: 230),
+              const SizedBox(height: 10),
+              const Text(
+                'Faça Seu Cadastro',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 32, 0, 66), // Cor laranja da logo
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  letterSpacing: 1.5,
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Digite sua senha',
+              SizedBox(height: 30),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite seu login',
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              width: 350,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(
-                    const Color.fromARGB(255, 17, 190, 83),
+              SizedBox(height: 15),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite sua senha',
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    print('Botão "Esqueceu sua senha?" pressionado!');
+                  },
+                  child: const Text(
+                    'Esqueceu sua senha?',
+                    style: TextStyle(
+                      color: Color.fromARGB(
+                        255,
+                        32,
+                        0,
+                        66,
+                      ), // Usa a mesma cor principal
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
-                onPressed: () {
-                  print(emailController.text);
-                  print(passwordController.text);
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.login),
-                    SizedBox(width: 10),
-                    Text('Salvar'),
-                  ],
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 350,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                      const Color.fromARGB(255, 32, 0, 66),
+                    ),
+                  ),
+                  onPressed: () {
+                    print(emailController.text);
+                    print(passwordController.text);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.login, color: Colors.white),
+                      SizedBox(width: 10),
+                      Text(
+                        'Salvar',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
